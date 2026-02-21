774551abe502be5e3484c34ddc03730c4e9e9a7c
unknown <shwetakumari3405@gmail.com>
2026-02-21 16:29:41 +0530

    [shweta]: added program file of UC2-InchEquality

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,25 +1,49 @@
 package com.apps.quantitymeasurement;
+
 import java.util.Scanner;
 
 public class QuantityMeasurementApp {
+
+    // --------Feet Equality --------
+    public static boolean compareFeet(double value1, double value2) {
+        Feet feet1 = new Feet(value1);
+        Feet feet2 = new Feet(value2);
+        return feet1.equals(feet2);
+    }
+
+    // -------- Inches Equality --------
+    public static boolean compareInches(double value1, double value2) {
+        Inches inch1 = new Inches(value1);
+        Inches inch2 = new Inches(value2);
+        return inch1.equals(inch2);
+    }
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
         try {
+            // -------- FEET INPUT --------
             System.out.print("Enter first value in feet: ");
-            double first = Double.parseDouble(input.nextLine());
+            double feetFirst = Double.parseDouble(input.nextLine());
 
             System.out.print("Enter second value in feet: ");
-            double second = Double.parseDouble(input.nextLine());
+            double feetSecond = Double.parseDouble(input.nextLine());
 
-            Feet feet1 = new Feet(first);
-            Feet feet2 = new Feet(second);
+            boolean feetResult = compareFeet(feetFirst, feetSecond);
+            System.out.println("Feet Equal (" + feetResult + ")");
 
-            boolean result = feet1.equals(feet2);
+            System.out.println("----------------------------------");
 
-            System.out.println("Equal (" + result + ")");
+            // -------- INCH INPUT --------
+            System.out.print("Enter first value in inches: ");
+            double inchFirst = Double.parseDouble(input.nextLine());
+
+            System.out.print("Enter second value in inches: ");
+            double inchSecond = Double.parseDouble(input.nextLine());
+
+            boolean inchResult = compareInches(inchFirst, inchSecond);
+            System.out.println("Inches Equal (" + inchResult + ")");
 
         } catch (NumberFormatException e) {
             System.out.println("Invalid input! Please enter numeric values only.");
