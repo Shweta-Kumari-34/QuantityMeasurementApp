c109e8f10e543a5a81b4959868adb031dd6893b4
unknown <shwetakumari3405@gmail.com>
2026-02-23 14:44:01 +0530

    [shweta] added modified function referenced due to refactor program structure

--- src/test/java/com/apps/quantitymeasurement/ExtendedUnitTest.java
+++ src/test/java/com/apps/quantitymeasurement/ExtendedUnitTest.java
@@ -7,41 +7,41 @@ public class ExtendedUnitTest {
 
     @Test
     void testEquality_YardToFeet_EquivalentValue() {
-        Length l1 = new Length(1.0, Length.LengthUnit.YARDS);
-        Length l2 = new Length(3.0, Length.LengthUnit.FEET);
+        Length l1 = new Length(1.0, LengthUnit.YARDS);
+        Length l2 = new Length(3.0, LengthUnit.FEET);
 
         assertTrue(l1.equals(l2));
     }
 
     @Test
     void testEquality_YardToFeet_NonEquivalentValue() {
-        Length l1 = new Length(1.0, Length.LengthUnit.YARDS);
-        Length l2 = new Length(2.0, Length.LengthUnit.FEET);
+        Length l1 = new Length(1.0, LengthUnit.YARDS);
+        Length l2 = new Length(2.0, LengthUnit.FEET);
 
         assertFalse(l1.equals(l2));
     }
 
     @Test
     void testEquality_CentimetersToInches_EquivalentValue() {
-        Length l1 = new Length(2.54, Length.LengthUnit.CENTIMETERS);
-        Length l2 = new Length(1.0, Length.LengthUnit.INCHES);
+        Length l1 = new Length(2.54, LengthUnit.CENTIMETERS);
+        Length l2 = new Length(1.0, LengthUnit.INCHES);
 
         assertTrue(l1.equals(l2));
     }
 
     @Test
     void testEquality_FeetToYard_EquivalentValue() {
-        Length l1 = new Length(3.0, Length.LengthUnit.FEET);
-        Length l2 = new Length(1.0, Length.LengthUnit.YARDS);
+        Length l1 = new Length(3.0, LengthUnit.FEET);
+        Length l2 = new Length(1.0, LengthUnit.YARDS);
 
         assertTrue(l1.equals(l2));
     }
 
     @Test
     void testEquality_AllUnits_ComplexScenario() {
-        Length l1 = new Length(2.0, Length.LengthUnit.YARDS);
-        Length l2 = new Length(6.0, Length.LengthUnit.FEET);
-        Length l3 = new Length(72.0, Length.LengthUnit.INCHES);
+        Length l1 = new Length(2.0, LengthUnit.YARDS);
+        Length l2 = new Length(6.0, LengthUnit.FEET);
+        Length l3 = new Length(72.0,LengthUnit.INCHES);
 
         assertTrue(l1.equals(l2));
         assertTrue(l2.equals(l3));
@@ -50,14 +50,14 @@ public class ExtendedUnitTest {
 
     @Test
     void testEquality_CentimetersSameReference() {
-        Length l1 = new Length(1.0, Length.LengthUnit.CENTIMETERS);
+        Length l1 = new Length(1.0, LengthUnit.CENTIMETERS);
 
         assertTrue(l1.equals(l1));
     }
 
     @Test
     void testEquality_CentimetersNullComparison() {
-        Length l1 = new Length(1.0, Length.LengthUnit.CENTIMETERS);
+        Length l1 = new Length(1.0, LengthUnit.CENTIMETERS);
 
         assertFalse(l1.equals(null));
     }
