54f04ba7045ab43a2f1b64007eea02f4c02054b2
unknown <shwetakumari3405@gmail.com>
2026-03-11 12:02:37 +0530

    [shweta]: added define IMeasurable interface for unit conversion and measurement type resolution

--- src/main/java/com/apps/quantitymeasurement/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -1,19 +1,46 @@
 package com.apps.quantitymeasurement;
 
 public interface IMeasurable {
-    double convertToBase(double value);
-    double convertFromBase(double baseValue);
-    String getCategory();
 
-    // UC14: Default true - Sab units (Length, Weight, Volume) arithmetic support karengi
-    default boolean supportsArithmetic() {
-        return true; 
+    double toBase(double value);
+
+    double fromBase(double baseValue);
+
+    String getMeasurementType();
+
+    String getUnitName();
+
+    default boolean supportsAddition() {
+        return true;
     }
 
-    // UC14: Helper to throw exception if not supported
-    default void validateOperationSupport(String operation) {
-        if (!supportsArithmetic()) {
-            throw new UnsupportedOperationException(getCategory() + " does not support " + operation);
+    default boolean supportsSubtraction() {
+        return true;
+    }
+
+    default boolean supportsDivision() {
+        return true;
+    }
+
+    static IMeasurable resolve(String measurementType, String unitName) {
+        if (measurementType == null || unitName == null) {
+            throw new IllegalArgumentException("Measurement type and unit cannot be null");
+        }
+
+        String type = measurementType.trim().toUpperCase();
+        String unit = unitName.trim().toUpperCase();
+
+        switch (type) {
+            case "LENGTH":
+                return LengthUnit.valueOf(unit);
+            case "WEIGHT":
+                return WeightUnit.valueOf(unit);
+            case "VOLUME":
+                return VolumeUnit.valueOf(unit);
+            case "TEMPERATURE":
+                return TemperatureUnit.valueOf(unit);
+            default:
+                throw new IllegalArgumentException("Unsupported measurement type: " + measurementType);
         }
     }
 }
