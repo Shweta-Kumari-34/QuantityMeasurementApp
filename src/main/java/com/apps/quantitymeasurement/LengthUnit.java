3ce8b74e618b3f476e086fce73dbad9c47042001
unknown <shwetakumari3405@gmail.com>
2026-03-11 12:06:51 +0530

    [shweta]: added implement LengthUnit enum with base inch conversion and measurement metadata

--- src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -1,29 +1,36 @@
 package com.apps.quantitymeasurement;
 
 public enum LengthUnit implements IMeasurable {
-    FEET(12.0), 
-    INCHES(1.0), 
-    YARDS(36.0), 
-    CENTIMETER(0.4); // 1 inch = 2.5 cm => 1 cm = 1/2.5 = 0.4 inches
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
 
-    private final double factor;
+    private final double conversionFactorToInches;
 
-    LengthUnit(double factor) {
-        this.factor = factor;
+    LengthUnit(double conversionFactorToInches) {
+        this.conversionFactorToInches = conversionFactorToInches;
     }
 
     @Override
-    public double convertToBase(double value) {
-        return value * factor; // Sab inches mein convert hoga
+    public double toBase(double value) {
+        return value * conversionFactorToInches;
     }
 
     @Override
-    public double convertFromBase(double baseValue) {
-        return baseValue / factor;
+    public double fromBase(double baseValue) {
+        return baseValue / conversionFactorToInches;
     }
 
     @Override
-    public String getCategory() {
+    public String getMeasurementType() {
         return "LENGTH";
     }
+
+    @Override
+    public String getUnitName() {
+        return name();
+    }
+
+	
 }
