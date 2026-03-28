3c1a7726d6ac0afcdcd3d2579f0538c79ff5543c
unknown <shwetakumari3405@gmail.com>
2026-03-28 14:23:22 +0530

    [shweta]: added OpenApiConfig file

--- src/main/java/com/app/quantitymeasurement/config/OpenApiConfig.java
+++ src/main/java/com/app/quantitymeasurement/config/OpenApiConfig.java
@@ -1,12 +1,14 @@
 package com.app.quantitymeasurement.config;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 /*
  * =========================================================
@@ -41,26 +43,54 @@ import io.swagger.v3.oas.models.info.License;
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
+	@Bean // Spring will create this object and manage it
     public OpenAPI quantityMeasurementAPI() {
 
         return new OpenAPI()
+                /*
+                 * =========================================================
+                 * API INFORMATION (VISIBLE IN SWAGGER UI)
+                 * =========================================================
+                 */
                 .info(new Info()
-                        .title("Quantity Measurement API")
+                        .title("Quantity Measurement API") // API name shown in Swagger
                         .description(
                                 "REST API for performing quantity measurement operations "
                                         + "such as comparison, conversion, addition, subtraction, "
                                         + "and division across different measurement types."
-                        )
-                        .version("1.0.0")
+                        ) // description at top
+                        .version("1.0.0")  // version tag
                         .contact(new Contact()
-                                .name("Quantity Measurement Team")
+                                .name("Quantity Measurement Team")  // contact info
                                 .email("support@quantitymeasurement.com")
                         )
                         .license(new License()
-                                .name("Apache 2.0")
+                                .name("Apache 2.0") // license info
                                 .url("https://www.apache.org/licenses/LICENSE-2.0.html")
                         )
-                );
+                        
+                    
+                )
+                /*
+                 * =========================================================
+                 *  SECURITY CONFIG (THIS ENABLES AUTHORIZE BUTTON)
+                 * =========================================================
+                 */
+                // "All APIs require BearerAuth unless specified"
+              .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+              
+              /*
+               * Define security scheme (JWT)
+               */
+              // defines What is BearerAuth?
+              .components(new Components()
+                  .addSecuritySchemes("BearerAuth",
+                        new SecurityScheme()
+                                .name("Authorization") // header name
+                                .type(SecurityScheme.Type.HTTP)  // HTTP auth
+                                .scheme("bearer") // Bearer token
+                                .bearerFormat("JWT") // format is JWT
+                   )
+              );
     }
 }
